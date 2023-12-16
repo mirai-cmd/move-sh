cfbac1977a09d8791e0fb2c93f232768ca788bca
ElliotAlderson-cmd <prajwalghotage2508@gmail.com>
2023-12-16 22:50:41 +0530

    Added help menu and output beautification

--- organise.sh
+++ organise.sh
@@ -1,8 +1,11 @@
 #!/bin/env bash
 
 #string constants
+script_name="Sh-move.it"
+readonly version="$script_name v1.0.0"
 readonly info_symbol="[+]"
 readonly error_symbol="[-]"
+
 declare -A summary=([files_transferred]=0 [folders_created]=0)
 declare -A files_per_folder
 declare src_dir
@@ -10,7 +13,19 @@ declare dst_dir
 declare excluded_extensions="-name '*.*'"
 declare delete_source_dir=false
 declare log_file
-while getopts 'rs:d:e:l:' OPTION; do
+
+function show_help() {
+    # Display Help
+    printf "Copies files from a source directory to a destination directory and organises them based on their extensions\n"
+    printf "\nUsage: ./organise.sh -s [SOURCE_DIR] -d [DST_DIR] [options]...\n"
+    printf "\nOptions:\n"
+    printf " -e \"extensions\"   Exclude file extensions. Enter a string of space-separated of extensions. Ex : \"txt sh\"\n"
+    printf " -l [file_name]    Generate log file with name [file_name]\n"
+    printf " -r%-15s Recursively delete source directory.\n"
+    printf " -h%-15s Print this Help.\n"
+    printf " -V%-15s Print software version and exit.\n"
+}
+while getopts 'Vhrs:d:e:l:' OPTION; do
     case "$OPTION" in
     s)
         src_dir="$OPTARG"
@@ -27,13 +42,25 @@ while getopts 'rs:d:e:l:' OPTION; do
     e)
         excluded_extensions="$OPTARG"
         ;;
+    h)
+        show_help
+        exit 0
+        ;;
+    V)
+        printf "$version"
+        exit 0
+        ;;
     ?)
-        echo "script usage: $(basename \ $0) -r -s src_dir -d dst_dir -e \"excluded extensions\"" >&2
+        echo "Use $(basename $0) -h for help" >&2
         exit 1
         ;;
     esac
 done
 shift "$(($OPTIND - 1))"
+
+figlet "$script_name" -k
+
+#Helper Functions
 function INFO() {
     echo "$info_symbol" "$1"
 }
@@ -71,12 +98,12 @@ if [ ! "$excluded_extensions" == "-name '*.*'" ]; then
     unset 'extension[-1]'
     extension+=(\))
 fi
-# echo "find "$src_dir" -type f $extension_string -exec echo "{}" \;"
-#-not \( -name '*.sh' -o -name '*.doc' \)
+
 file_paths=$(find "$src_dir" -type f "${extension[@]}" -exec echo "{}" \;)
 
 INFO "Copying files from $src_dir to $dst_dir"
-#create extension-wise directories and copy into them
+
+#create extension-wise directories and copy into them, and handle duplicate file_names
 for file_path in $file_paths; do
     filename=$(basename "$file_path")
     extension="${filename##*.}"
@@ -90,6 +117,7 @@ for file_path in $file_paths; do
         ((files_per_folder[$extension]++))
         res=$(cp "$file_path" "$destination_path/$name(${files_per_folder[$extension]}).$extension")
         ((summary[files_transferred]++))
+        printf "[ $(date) ][INFO] Copied %-60s\t--->\t%-60s\n" "$file_path" "$destination_path" >>"$current_log_file"
         continue
     fi
     res=$(cp "$file_path" "$destination_path")
@@ -97,12 +125,16 @@ for file_path in $file_paths; do
     ((summary[files_transferred]++))
     printf "[ $(date) ][INFO] Copied %-60s\t--->\t%-60s\n" "$file_path" "$destination_path" >>"$current_log_file"
 done
-if [ "$delete_source_dir" == true ]; then
+
+#Delete source drectory
+if [ "$dlete_source_dir" == true ]; then
     INFO "Deleting files from $src_dir..."
     $(rm -rf "$src_dir")
 fi
+
+#Summary
 INFO "Reorgnisation complete"
-INFO "Check $current_log_file for more details" 
+INFO "Check $current_log_file for more details"
 echo -e "\n--------Summary--------\n"
 INFO "Folders created : ${summary[folders_created]}"
 INFO "Files transferred : ${summary[files_transferred]}"
@@ -115,3 +147,4 @@ for key in "${!files_per_folder[@]}"; do
     printf "\n"
 done
 echo "+----------------------------+"
+cowsay -e oO "I like to sh-move it sh-move it (pun intended)"
